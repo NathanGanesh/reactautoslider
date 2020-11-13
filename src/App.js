976333833fa76dd75a2150c5f9df3c6c5d5507fc
nathan ganesh <489290@student.saxion.nl>
2020-11-13 13:11:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+function App() {
+	return (
+		<main>
+			<h2>
+				<span className="slide">/</span> Reviews
+			</h2>
+		</main>
+	);
+}
+
+export default App;
